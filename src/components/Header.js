6d6de96a90d86f44fae762150ab64cfcdac0565e
Abdela Umer <adheremo65@gmail.com>
2023-06-04 12:12:24 -0400

    react-hooks-information-flow-lab

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+function Header({ onDarkModeClick }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  );
+}
+export default Header;
